edf952d64b22705dd5f5bcdd179ec149a179ee31
Zaki <muhammadfarid.zaki@gmail.com>
2023-05-16 11:55:49 +0700

    added menu screen (view&vm)

--- src/pages/menu/index.tsx
+++ src/pages/menu/index.tsx
@@ -8,60 +8,134 @@ import {
   ScrollView,
   SafeAreaView,
   StyleSheet,
+  FlatList,
+  ListRenderItem,
+  TouchableOpacity,
 } from 'react-native';
-import React, {useEffect} from 'react';
+import React, {useCallback, useEffect, useMemo, useState} from 'react';
 import useVM from './menu.vm';
-import Header from '../../components/Header';
-import Footer from '../../components/Footer';
-import {Background, Grey} from '../../assets/theme/Colors';
+import Header from '../../components/header/Header';
+import Footer from '../../components/footer/Footer';
+import {
+  Background,
+  BorderGrey,
+  DimGrey,
+  Grey,
+  GreyDesc,
+  GreyWhite,
+  White,
+} from '../../assets/theme/Colors';
 import {ms} from 'react-native-size-matters';
 import Divider from '../../components/Divider';
-import {Sort} from '../../assets/icons';
+import {IconMobile} from '../../assets/icons';
 import {Typography} from '../../assets/theme/Typography';
-import Button from '../../components/SortButton';
-import SortButton from '../../components/SortButton';
+import Button from '../../components/Button';
 import Row from '../../components/Row';
+import Spacer from '../../components/Spacer';
+import ProductCard from '../../components/ProductCard';
+import Loader from '../../components/loader/Loader';
+import Popup from '../../components/popup/Popup';
+import SortContainer from '../../components/sort/SortContainer';
 
 const Menu: React.FC = () => {
-  return (
-    <SafeAreaView
-      style={{
-        flex: 1,
-        backgroundColor: Background,
-      }}>
-      <Header count={3} />
-      <View style={styles.mainContainer}>
-        <View style={styles.sortContainer}>
-          <Row>
-            <Image style={styles.sort} source={Sort} />
-            <Text style={[Typography.h2, {color: Grey, marginLeft: ms(8)}]}>
-              Sort By:
-            </Text>
-          </Row>
-          <SortButton title='Default'/>
-        </View>
-        <Divider />
+  const {
+    productData,
+    fetchProductList,
+    loading,
+    error,
+    count,
+    sort,
+    updateAmount,
+    getTotal,
+    getQuantity,
+    modalVisible,
+    setModalVisible,
+    visible,
+    setVisible,
+    resetTransaction,
+    closeSort,
+    sortName,
+    setSortName,
+  } = useVM();
+  const total = getTotal(productData);
+  useEffect(() => {
+    fetchProductList();
+  }, []);
+  const renderLoading = useMemo(() => <Loader />, [loading]);
+  const renderHeader = useCallback(
+    (count: number | null) => (
+      <View style={{backgroundColor: White}}>
+        <Header count={count} />
+        {productData.length > 0 && (
+          <SortContainer
+            setSortName={setSortName}
+            sortName={sortName}
+            visible={visible}
+            setVisible={() => setVisible(!visible)}
+            sort={sort}
+          />
+        )}
       </View>
-      <Footer />
+    ),
+    [loading, sort, productData],
+  );
+  const renderItem: ListRenderItem<any> = useCallback(
+    ({item, index}) => {
+      return (
+        <>
+          {index === 0 && <Spacer length={ms(32)} />}
+          <ProductCard updateAmount={(x,y)=>{updateAmount(x,y);closeSort()}} item={item} />
+        </>
+      );
+    },
+    [updateAmount],
+  );
+  return (
+    <SafeAreaView style={styles.mainContainer}>
+      <FlatList
+
+        showsVerticalScrollIndicator={false}
+        ListHeaderComponent={renderHeader(count)}
+        stickyHeaderIndices={[0]}
+        contentContainerStyle={(productData.length>0)?{
+          flexGrow: 1,
+          paddingBottom:  total ? ms(226) : ms(173),
+        }:{height:'100%',flexGrow:1}}
+        data={productData}
+        ItemSeparatorComponent={() => <Spacer length={ms(42)} />}
+        renderItem={renderItem}
+        initialNumToRender={10}
+        keyExtractor={(item, index) => String(index)}
+        ListEmptyComponent={renderLoading}
+        extraData={productData}
+      />
+      {!loading && (
+        <Footer
+          total={total}
+          onPressCheckout={() => setModalVisible(!modalVisible)}
+          resetTransaction={resetTransaction}
+        />
+      )}
+      <Popup
+        quantity={getQuantity(productData)}
+        total={total}
+        modalVisible={modalVisible}
+        checkout={() => resetTransaction()}
+        setModalVisible={setModalVisible}
+      />
+      <Popup
+        isCheckingout={false}
+        modalVisible={loading}
+        checkout={() => resetTransaction()}
+        setModalVisible={setModalVisible}
+      />
     </SafeAreaView>
   );
 };
 const styles = StyleSheet.create({
   mainContainer: {
-    paddingHorizontal: ms(20),
-    paddingTop: ms(9),
-  },
-  sortContainer: {
-    paddingVertical: ms(23),
-    flexDirection: 'row',justifyContent:'space-between'
-  },
-  row: {
-    flexDirection: 'row',
-    alignItems: 'center',
-  },
-  sort: {
-    width: ms(12),
-    height: ms(12),
+    flex: 1,
+    backgroundColor: Background,
   },
 });
 export default Menu;
--- src/pages/menu/menu.vm.ts
+++ src/pages/menu/menu.vm.ts
@@ -1,27 +1,111 @@
-import { useEffect } from 'react';
-import { useState } from 'react';
+
+import { useEffect, useState } from 'react';
+
 import { BASE_API } from '../../api';
 
 
-export default function FaqDetailViewModel() {
-    const [faqData, setFaqData] = useState(null);
-    const [loading, setLoading] = useState(false);
-    const [pertanyaan, setPertanyaan] = useState('');
-    const [jawaban, setJawaban] = useState('');
-    const [updating, setUpdating] = useState(false);
-    const [error, setError] = useState(null);
-    const [visible, setVisible] = useState(false);
-    const [visibleModal, setVisibleModal] = useState(false);
-    const startUpdating = () => setUpdating(true);
-    const finishUpdating = () => setUpdating(false);
-    const openMenu = () => setVisible(true);
+export default function MainTabViewModel() {
+    const [productData, setProductData] = useState<any>([]);
+    const [count, setCount] = useState<any>(null);
+    const [loading, setLoading] = useState<any>(false);
+    const [error, setError] = useState<any>(null);
+    const [visible, setVisible] = useState(false)
+    const [modalVisible, setModalVisible] = useState(false)
+    const [sortName, setSortName] = useState('Default')
 
-    const closeMenu = () => setVisible(false);
 
-    const showModal = () => setVisibleModal(true);
-    const hideModal = () => setVisibleModal(false);
+    const fetchProductList = async () => {
+        try {
+            setLoading(true);
+            const response = await fetch(
+                `${BASE_API}`
+            );
+            const json: { limit: any, products: any } = await response.json();
+            setCount(json.limit)
+            setProductData(json.products.map((x: { id: number, title: string, price: number, stock: number }) => ({ id: x.id, title: x.title, price: x.price, stock: x.stock, amount: 0 })))
 
-    return {
-
+        } catch (error: any) {
+            setError(error.message);
+        } finally {
+            setLoading(false);
+        }
     };
+    const closeSort = () => {
+        setVisible(false)
+    }
+    const resetTransaction = () => {
+        fetchProductList()
+        closeSort()
+        setSortName('Default')
+    };
+    const updateAmount = (id: number, amount: number) => {
+        const newProductData = productData.map((obj: { id: number, title: string, price: number, stock: number, amount: number }) => {
+            if (obj.id === id) {
+                return { ...obj, amount: obj.amount + amount };
+            }
+            return obj;
+        });
+
+        setProductData(newProductData);
+    };
+    const sort = (sort: number) => {
+        function compareNameAsc(a: any, b: any) {
+            if (a.title < b.title) {
+                return -1;
+            }
+            if (a.title > b.title) {
+                return 1;
+            }
+            return 0;
+        }
+        function compareNameDesc(a: any, b: any) {
+            if (a.title > b.title) {
+                return -1;
+            }
+            if (a.title < b.title) {
+                return 1;
+            }
+            return 0;
+        }
+        function comparePriceAsc(a: any, b: any) {
+            if (a.price > b.price) {
+                return -1;
+            }
+            if (a.price < b.price) {
+                return 1;
+            }
+            return 0;
+        }
+        function comparePriceDesc(a: any, b: any) {
+            if (a.price < b.price) {
+                return -1;
+            }
+            if (a.price > b.price) {
+                return 1;
+            }
+            return 0;
+        }
+
+        setProductData([...productData].sort(sort === 0 ? comparePriceAsc : sort === 1 ? comparePriceDesc : sort === 2 ? compareNameAsc : compareNameDesc));
+    };
+    const getTotal = (x: { id: number, title: string, price: number, stock: number, amount: number }[]) => {
+        let total = 0;
+
+        for (let i = 0; i < x.length; i++) {
+            const obj: { id: number, title: string, price: number, stock: number, amount: number } = x[i];
+            const result = obj.price * obj.amount;
+            total += result;
+        }
+        return total
+    }
+    const getQuantity = (x: { id: number, title: string, price: number, stock: number, amount: number }[]) => {
+        let quantity = 0;
+
+        for (let i = 0; i < x.length; i++) {
+            const obj: { id: number, title: string, price: number, stock: number, amount: number } = x[i];
+            quantity += obj.amount;
+        }
+        return quantity
+    }
+    return { productData, loading, error, fetchProductList, count, sort, updateAmount, getTotal, getQuantity, modalVisible, setModalVisible, visible, setVisible, resetTransaction, closeSort, sortName, setSortName };
 }
